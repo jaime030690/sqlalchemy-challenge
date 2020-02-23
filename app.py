8fbd74bfde16ffefaa234eda0966b2d313194339
Jaime Echevarria <jaime030690@gmail.com>
2020-02-23 17:29:46 -0500

    completed api

--- app.py
+++ app.py
@@ -22,6 +22,13 @@ Station = Base.classes.station
 # Setup flask
 app = Flask(__name__)
 
+# Pull last date
+session = Session(engine)
+
+# Query to pull last date
+last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+session.close()
+
 # Flask routes
 @app.route('/')
 def home():
@@ -42,19 +49,19 @@ def precipitation():
     session = Session(engine)
 
     # Query to pull date and prcp
-    query = session.query(Measurement.date, Measurement.prcp).all()
+    prcp_query = session.query(Measurement.date, Measurement.prcp).all()
     session.close()
 
     # Will return this
-    results = []
+    prcp_results = []
     
     # Look thru query, use date as key and prcp as value, and add dict to list
-    for d, p in query:
+    for d, p in prcp_query:
         climate_dict = {}
         climate_dict[d] = p
-        results.append(climate_dict)
+        prcp_results.append(climate_dict)
 
-    return jsonify(results)
+    return jsonify(prcp_results)
 
 @app.route('/api/v1.0/stations')
 def stations():
@@ -63,20 +70,20 @@ def stations():
     session = Session(engine)
 
     # Query to pull station and name
-    query = session.query(Station.station, Station.name).all()
+    stations_query = session.query(Station.station, Station.name).all()
     session.close()
 
     # Will return this
-    results = []
+    stations_results = []
 
     # Look thru query, use station and name as keys and apply the corresponding values, add to list
-    for s, n in query:
+    for s, n in stations_query:
         station_dict = {}
         station_dict['station'] = s
         station_dict['name'] = n
-        results.append(station_dict)
+        stations_results.append(station_dict)
 
-    return jsonify(results)
+    return jsonify(stations_results)
 
 @app.route('/api/v1.0/tobs')
 def tobs():
@@ -84,45 +91,62 @@ def tobs():
     # Setup engine
     session = Session(engine)
 
-    # Query to pull last date
-    query = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    session.close()
-
     # Calculate date for one year ago
-    y, m, d = [int(item) for item in query[0].split("-")]
+    y, m, d = [int(item) for item in last_date.split("-")]
     cutoff = dt.datetime(y-1, m, d)
 
     # Query to pull tobs
-    query = session.query(Measurement.tobs).filter(func.strftime(Measurement.date) >= cutoff).all()
+    tobs_query = session.query(Measurement.tobs).filter(func.strftime(Measurement.date) >= cutoff).all()
     session.close()
 
     # Return this
-    results = []
+    tobs_results = []
 
     # Convert query to a list
-    results = list(np.ravel(query))
+    tobs_results = list(np.ravel(tobs_query))
 
-    return jsonify(results)
+    return jsonify(tobs_results)
     
 @app.route('/api/v1.0/<start>')
 def start(start):
 
-    '''
-    TODO:
-    Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-    When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+    # Get query
+    start_query = calc_temps(start, last_date)
 
-    '''
+    # Return this
+    start_results = []
+    
+    # Convert query to a list
+    start_results = list(np.ravel(start_query))
+    
+    return jsonify(start_results)
+    
 
 @app.route('/api/v1.0/<start>/<end>')
 def start_end(start, end):
 
-    '''
-    TODO:
-    Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-    When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+    # Get query
+    start_end_query = calc_temps(start, end)
 
-    '''
+    # Return this
+    start_end_results = []
+
+    # Convert query to a list
+    start_end_results = list(np.ravel(start_end_query))
+
+    return jsonify(start_end_results)
+
+def calc_temps(start_date, end_date):
+    
+    # Setup engine
+    session = Session(engine)
+    
+    # Query to get temperature stats
+    stats_query = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    session.close()
+
+    return stats_query
 
 if __name__ == '__main__':
     app.run(debug=True)
